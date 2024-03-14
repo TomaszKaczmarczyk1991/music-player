16331d7d34e5539fbb7dd055d24081eab919825c
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-03-15 00:00:50 +0100

    init task/volume

--- script.js
+++ script.js
@@ -282,6 +282,13 @@ function setProgressBar(e) {
   music.currentTime = (clickX / width) * duration;
 }
 
+// Set Volume Bar
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    console.log(volume);
+    if (volume < 0.1) volume = 0;
+    if (volume > 0.9) volume = 1;
+}
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
@@ -289,3 +296,4 @@ nextBtn.addEventListener('click', nextSong);
 music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
 progressContainer.addEventListener('click', setProgressBar);
+volumeRange.addEventListener('click', changeVolume);
